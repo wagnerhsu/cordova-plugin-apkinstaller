87bba7885f0b4581e168f85ab4d770b0c80ff312
Wagner Xu <wagnerhsu@hotmail.com>
2022-10-27 14:26:57 +0800

    remove android.support.v4

--- README.Wagner.md
+++ README.Wagner.md
@@ -1,5 +1,9 @@
 # README
 
+## 2022-10-27
+
+- Update to androidx
+
 ## 2022-09-02
 
 * Add this file
--- src/android/Installer.java
+++ src/android/Installer.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 
+import androidx.core.content.FileProvider;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -12,7 +13,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.os.Environment;
-import android.support.v4.content.FileProvider;
 import android.text.TextUtils;
 
 import java.io.File;
